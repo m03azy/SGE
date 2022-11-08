0b37017df517369625d06fa7ab34282ede637b8d
m03azy <iammosesfaustin@gmail.com>
2022-11-08 20:07:48 +0300

    initial poject baseline

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <html>
     <head>
         <link href='css/style.css' type='text/css' rel='stylesheet'>
+        <meta charset='utf-8'>
+          <!-- CSS only -->
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </head>
     <body>
         <img src="images/logo.png" length='200px' widht='150px'>
-        <h1> Simple Gallery E-commerce</h1><br>            
-            <a href=index.php>home</a>
-            <a href=index.php>about</a>
-            <a href=index.php>contacts</a>    
+        <h1> Simple Gallery E-commerce</h1><br>
+        <nav class="navbar navbar-expand-sm bg-light">
+            <div class="container-fluid">
+                <!-- Links -->
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ndex.php">contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ndex.php">about</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+  
         <hr>
         <?php
             $products = array(
@@ -48,20 +68,26 @@
             $price=$_GET['price'];
             
         ?>
-        <?php           
-            if(count($products) > 0 ){
-                 for($i = 0; $i < count($products); $i++){ ?>
-                
-                 <div class='row'>
-                    <img src='<?=$products[$i]['image']; ?>' length='200' width='150' /><br>
-                    <?= $products[$i]['name']; ?><br>
-                    the price is <?= $products[$i]['price'] ?><br>
-                    <a href='view.php?id=<?=$i; ?>'><button>order now</button></a><br>
-                 </div>
-                <?php }
-            }
-            $products=$_GET='view.php?'
-        ?>
+        <div class='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
+            <?php           
+                if(count($products) > 0 ){
+                    
+                    for($i = 0; $i < count($products); $i++){ ?>
+                    
+                    
+                        <div class='col'>
+                            <div class='card'>
+                                <img src='<?=$products[$i]['image']; ?>' length='200' width='150' /><br>
+                                <?= $products[$i]['name']; ?><br>
+                                the price is <?= $products[$i]['price'] ?><br>
+                                <a href='view.php?id=<?=$i; ?>'><button type='botton' class='btn btn-primary'>order now</button></a><br>
+                            </div>
+                        </div>        
+                    <?php }
+                }
+                $products=$_GET='view.php?'
+            ?>
+        </div>  
         <hr>
         <footer> have a good day</footer>
     </body>
--- view.php
+++ view.php
@@ -1,6 +1,11 @@
 <html>
     <head>
         <link href='css/style.css' type='text/css' rel='stylesheet'>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </head>
     <body>
         
@@ -57,11 +62,11 @@
         ?>
         
         <form action='preview.php' method='post'>
-            <div >
+            <div class='comtainer-md'>
                 <img src='<?=$products[$id]['image']; ?>' length='400' width='300'><br>
                 <?= $products[$id]['name']; ?> : get by <?= $products[$id]['price']; ?><br>
-                <input type='text' name='fname' placeholder='first name' required='required'>first name</input><br>
-                <input type='text' name='lname' placeholder='last name' required='required'></input>last name<br>
+                <input type='text' name='fname' placeholder='first name' required='required'></input><br>
+                <input type='text' name='lname' placeholder='last name' required='required'></input><br>
                 <input type='email' name='email' placeholder='your email' required='required'></input><br>                          
                 <input type='text' name='card' placeholder='card number' required='required'></input><br>
                 <button name='submit' type='submit'>submit</button>
